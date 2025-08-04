ab9b83e38c0fdc5a0d5c187fd709d8766be64930
Summer <summer.flowers94@gmail.com>
2025-08-04 18:49:01 -0500

    Navbar header positioning
    Complete static navbar with all elements in their designated position (non-mobile screen only). Ensure font, color, and hover styles are in place

--- startdev/src/Header/Header.js
+++ startdev/src/Header/Header.js
@@ -5,9 +5,31 @@ import { Link } from 'react-router-dom';
 function Header() {
   return (
     <div className="header">
-        <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
-          <p>thinkToProd</p>
-        </Link>
+        <div id="nav-logo">
+          <p>CURATED</p>
+        </div>
+        <div id="nav-links">
+          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+            <p>Home</p>
+          </Link>
+          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+            <p>Articles</p>
+          </Link>
+          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+            <p>Collections</p>
+          </Link>
+          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+            <p>About</p>
+          </Link>
+          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+            <p>Contact</p>
+          </Link>
+        </div>
+        <div >
+          <Link id="subscribe-btn" to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+            Subscribe
+          </Link>
+        </div>
     </div>
   );
 }
