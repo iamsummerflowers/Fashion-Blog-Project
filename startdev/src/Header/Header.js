fb90fa6b43fa2b16dfca8bbc670d905c020c0a20
Summer <summer.flowers94@gmail.com>
2025-08-05 19:27:27 -0500

    Responsive navbar
     next focus will be on ensuring this component is responsive to mobile screens (incorporate hamburger menu)

--- startdev/src/Header/Header.js
+++ startdev/src/Header/Header.js
@@ -1,36 +1,90 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdOutlineClose } from "react-icons/md";
+
+
 
 function Header() {
+  
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
+
+  const toggleMobileNav = () => {
+    setMobileNavOpen(!mobileNavOpen);
+  };
+
+
   return (
-    <div className="header">
-        <div id="nav-logo">
-          <p>CURATED</p>
+    <nav className="nav-container">
+      <div className="header-mobile">
+        <div onClick={toggleMobileNav} className={`${mobileNavOpen  ? 'hide' : 'mobile-menu-toggle1'}`}>
+          <p id="open" >
+            <GiHamburgerMenu/>
+          </p>
         </div>
-        <div id="nav-links">
-          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
-            <p>Home</p>
-          </Link>
-          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
-            <p>Articles</p>
-          </Link>
-          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
-            <p>Collections</p>
-          </Link>
-          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
-            <p>About</p>
-          </Link>
-          <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
-            <p>Contact</p>
-          </Link>
+        <div className={`${mobileNavOpen  ? 'header-mobileOpen' : 'hide'}`}>
+          <div className="nav-logo">
+            <p>CURATED</p>
+            <MdOutlineClose className="mobile-menu-toggle2" onClick={toggleMobileNav}/>
+          </div>
+          <div className="nav-links">
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Home</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Articles</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Collections</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>About</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Contact</p>
+            </Link>
+          </div>
+          <div className="subscribe-btn-container">
+            <Link className="subscribe-btn" to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              Subscribe
+            </Link>
+          </div>
         </div>
-        <div >
-          <Link id="subscribe-btn" to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
-            Subscribe
-          </Link>
-        </div>
-    </div>
+
+      </div>
+
+
+
+      <div className="header">
+
+          <div className="nav-logo">
+            <p>CURATED</p>
+          </div>
+          <div className="nav-links">
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Home</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Articles</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Collections</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>About</p>
+            </Link>
+            <Link to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              <p>Contact</p>
+            </Link>
+          </div>
+          <div>
+            <Link className="subscribe-btn" to="https://thinktoprod.com" target="_blank" rel="noopener noreferrer">
+              Subscribe
+            </Link>
+          </div>
+      </div>
+    </nav>
   );
 }
 
