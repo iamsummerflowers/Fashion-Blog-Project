faa3c284de21bd9421c4573cf8e169fad40964d6
Summer <summer.flowers94@gmail.com>
2025-08-10 21:34:13 -0500

    PopUp Articles
    logic in place, still needs fixes on the onClick loop

--- startdev/src/Main/Card.js
+++ startdev/src/Main/Card.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect, lazy, Suspense  } from 'react';
+
 import './Card.css';
+import ArticlePop1 from './ArticlePop1.js';
 import { HashLink } from 'react-router-hash-link';
 
 
-function Card({ id, cardImage, header, tag, blurb, author, date }) {
+function Card({ id, cardImage, header, tag, blurb, author, date, articleComponent }) {
+
+  const ArticleComponent = lazy(() => import(`./${articleComponent}`));
+
+
+  const [articleOpen, setArticleOpen] = useState(false);
+
+  const toggleArticle = () => {
+    setArticleOpen(!articleOpen);
+  };
+
   return (
     <div className="card" id={id}>
         <div className={`cardImage ${cardImage}`}>
@@ -15,10 +27,15 @@ function Card({ id, cardImage, header, tag, blurb, author, date }) {
           <p className="cardAuthor">{author}</p>
           <p className="cardDate">{date}</p>
         </div>
-        <div className="article-btn-container">
-          <HashLink className="article-btn" target="_self" rel="noopener noreferrer">
+        <div className="article-btn-container" >
+          <HashLink onClick={toggleArticle} className="article-btn" target="_self" rel="noopener noreferrer" >
             Read More
           </HashLink>
+        </div>
+        <div className={`${articleOpen  ? 'articlePop' : 'hide'}`}>
+          <Suspense fallback={<div>...</div>}>
+            <ArticleComponent />
+          </Suspense>
         </div>
     </div>
   );
--- startdev/src/Main/Main.js
+++ startdev/src/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Main.css';
 import Card from './Card.js';
+import ArticlePop1 from './ArticlePop1.js';
 import logo from '../assets/logoBrain.svg'
 
 
@@ -13,13 +14,14 @@ function Main() {
       </div>
       <div className="holdsAllCards">
           <Card 
-            id="firstCard"
+            id="                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  "
             header="Before you start..."
             tag=" “tag” "
             blurb="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. "
             author="John Smith"
             date="Dec 15, 2025"
             cardImage="cardImage1"
+            articleComponent="ArticlePop1"
           />
           <Card 
             id="secondCard"
@@ -29,6 +31,7 @@ function Main() {
             author="John Smith"
             date="Dec 15, 2025"
             cardImage="cardImage2"
+            articleComponent="ArticlePop1"
           />
           <Card 
             id="thirdCard"
@@ -38,6 +41,7 @@ function Main() {
             author="John Smith"
             date="Dec 15, 2025"
             cardImage="cardImage3"
+            articleComponent="ArticlePop1"
           />
       </div>
     </div>
