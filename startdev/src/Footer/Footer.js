5ffbed27f811cb41ba5b86195cfc84bbc773bc0c
Summer <summer.flowers94@gmail.com>
2025-08-14 15:54:58 -0500

    Responsive Footer
    footer structure is now responsive

--- startdev/src/Footer/Footer.js
+++ startdev/src/Footer/Footer.js
@@ -5,6 +5,8 @@ import { VscGithub } from "react-icons/vsc";
 import { MdOutlineEmail } from "react-icons/md";
 import { MdOutlineLocalPhone } from "react-icons/md";
 import { MdOutlineLocationOn } from "react-icons/md";
+import { VscTwitter } from "react-icons/vsc";
+import { FaInstagram } from "react-icons/fa";
 
 
 
@@ -15,69 +17,67 @@ function Footer() {
 
         <div className="footerMain">
             <div className="footerColumn">
-              <h4 className="footerColumnHeader">Column Title</h4>
-              <p className="footerBlurb">First column blurb</p>
+              <h4 className="footerColumnHeader" id="Curated">CURATED</h4>
+              <p className="footerBlurb">Discover thoughtfully selected content for curious minds.</p>
               <div className="footerSocials">
                 <Link className="footerSocialLink" id="gitHub" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
                   <VscGithub />
                 </Link>
                 <Link className="footerSocialLink" id="twitter" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                  <VscGithub />
+                  <FaInstagram />
                 </Link>
                 <Link className="footerSocialLink" id="instagram" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                  <VscGithub />
+                  <VscTwitter />
                 </Link>
               </div>
             </div>
             <div className="footerColumn">
-              <h4 className="footerColumnHeader">Column Title</h4>
+              <h4 className="footerColumnHeader">Catergories</h4>
               <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
+                Technology
               </Link>
               <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
+                Lifestyle
               </Link>
               <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
+                Wellness
               </Link>
               <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
+                Arts & Culture
               </Link>
               <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
-              </Link>
-            </div>
-            <div className="footerColumn">
-              <h4 className="footerColumnHeader">Column Title</h4>
-              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
-              </Link>
-              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
-              </Link>
-              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
-              </Link>
-              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
-              </Link>
-              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
-                Second Column Link
+                Science
               </Link>
             </div>
             <div className="footerColumn">
-              <h4 className="footerColumnHeader">Column Title</h4>
+              <h4 className="footerColumnHeader">Quick Links</h4>
+              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
+                About Us
+              </Link>
+              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
+                Contact
+              </Link>
+              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
+                Contribute
+              </Link>
+              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
+                Privacy Policy
+              </Link>
+              <Link className="footerLink" to="https://github.com/iamsummerflowers" target="_blank" rel="noopener noreferrer">
+                Terms of Service
+              </Link>
+            </div>
+            <div className="footerColumn">
+              <h4 className="footerColumnHeader">Contact Us</h4>
               <div className="footerContactInfo">
-                <MdOutlineEmail className="footerIcon"/>
-                <p>Email Address</p>
+                
+                <p> <MdOutlineEmail className="footerIcon"/> info@curated.com</p>
               </div>
               <div className="footerContactInfo">
-                <MdOutlineLocalPhone className="footerIcon"/>
-                <p>Phone Number</p>
+                <p><MdOutlineLocalPhone className="footerIcon"/> +1(555) 123-4567</p>
               </div>
               <div className="footerContactInfo">
-                <MdOutlineLocationOn className="footerIcon"/>
-                <p>Physical Address</p>
+                <p> <MdOutlineLocationOn className="footerIcon"/> 123 Content Steet Digtal City</p>
               </div>
             </div>
           </div>
